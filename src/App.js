54428f05bc11a0bc25ea094cc37c467f82dd2172
Kirill Kuklin <kirill.km.work@gmail.com>
2022-10-21 15:03:49 +0300

    add filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/base.css";
 import "./styles/main.css";
 
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
@@ -9,17 +9,29 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/postForm";
 import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
-// ЧАС/МИНУТА : 12 - минута
+// ЧАС/МИНУТА : 23 - минута
 
 function App() {
   const [posts, setPosts] = useState([
-    // { id: 1, title: "Javascript", body: "Description" },
-    // { id: 2, title: "Javascript", body: "Description" },
-    // { id: 3, title: "Javascript", body: "Description" },
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript", body: "Description" },
+    { id: 3, title: "Javascript", body: "Description" },
   ]);
-  const [selectedSort, setSelectedSort] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
+
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortAndSearchPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -29,36 +41,15 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   };
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder={'Поиск...'}
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Сортировка"
-          options={[
-            { value: 'title', name: 'По заголовку' },
-            { value: 'body', name: 'По описанию' }
-          ]}
-        />
-      </div>
-      {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title="Посты по JS" />
-        : <h2 style={{ textAlign: 'center' }}>Посты не найдены!</h2>
-      }
-
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortAndSearchPosts} title="Посты по JS" />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, title, remove }) => {
-
+  if (!posts.length) {
+    return (<h2 style={{ textAlign: 'center' }}>Посты не найдены!</h2>)
+  }
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
